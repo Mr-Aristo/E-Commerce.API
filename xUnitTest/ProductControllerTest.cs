43c3f91c31a2b8f658396eeb00f47d8b40c09b92
Gecher Emre <emre.gecher@indusoft.ru>
2024-06-07 08:13:00 +0300

    Unit Test added

--- xUnitTest/ProductControllerTest.cs
+++ xUnitTest/ProductControllerTest.cs
@@ -17,40 +17,6 @@ namespace xUnitTest
         /// Controllesr asnyc oldugu icin test fonk da async olmali.
         /// </summary>
         /// <returns></returns>
-       // [Fact]
-        //public async Task Get_ReturnsPaginatedProducts()
-        //{
-        //    // Arrange
-        //    var mockProductRead = new Mock<IProductReadRepository>();
-        //    var pagination = new Pagination { Page = 1, Size = 10 };
-
-        //    var products = new List<Product>
-        //{
-        //    new Product { Id = Guid.NewGuid(), Name = "Product1", Stock = 100, Price = 10, CreationDate = DateTime.Now, UpdateDate = DateTime.Now }
-        //};
-
-        //    mockProductRead.Setup(service => service.GetAll(false)).Returns(products.AsQueryable());
-
-        //    var controller = new MyTestController(mockProductRead.Object, null, null, null);
-
-        //    // Act
-        //    var result = await controller.Get(pagination);
-
-        //    // Assert
-        //    var okResult = Assert.IsType<OkObjectResult>(result);
-        //    var returnedValue = okResult.Value as dynamic;
-
-        //    Assert.Equal(products.Count, (int)returnedValue.totalCount);
-        //    Assert.Equal(products.Select(p => new
-        //    {
-        //        p.Id,
-        //        p.Name,
-        //        p.Stock,
-        //        p.Price,
-        //        p.CreationDate,
-        //        p.UpdateDate
-        //    }).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Count(), ((IEnumerable<dynamic>)returnedValue.products).Count());
-        //}
         [Fact]
         public async Task Get_ReturnsPaginatedProducts()
         {
@@ -62,14 +28,13 @@ namespace xUnitTest
    
 
             var pagination = new Pagination { Page = 1, Size = 10 };
-            var productId = Guid.NewGuid();
             var products = new List<Product>
         {
-            new Product { Id = productId, Name = "Product1", Stock = 100, Price = 10, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new Product { Id = productId, Name = "Product2", Stock = 200, Price = 20, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new Product { Id = productId, Name = "Product3", Stock = 300, Price = 30, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new Product { Id = productId, Name = "Product4", Stock = 400, Price = 40, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new Product { Id = productId, Name = "Product5", Stock = 500, Price = 50, CreationDate = DateTime.Now, UpdateDate = DateTime.Now }
+            new Product { Id = new Guid("d91bc910-7a21-43c2-8e18-5fce8fa960ae"), Name = "Product1", Stock = 100, Price = 10, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
+            new Product { Id = new Guid("d91bc910-7a22-43c2-8e18-5fce8fa961ae"), Name = "Product2", Stock = 200, Price = 20, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
+            new Product { Id = new Guid("d91bc910-7a23-43c2-8e18-5fce8fa962ae"), Name = "Product3", Stock = 300, Price = 30, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
+            new Product { Id = new Guid("d91bc910-7a24-43c2-8e18-5fce8fa963ae"), Name = "Product4", Stock = 400, Price = 40, CreationDate = DateTime.Now, UpdateDate = DateTime.Now },
+            new Product { Id = new Guid("d91bc910-7a25-43c2-8e18-5fce8fa964ae"), Name = "Product5", Stock = 500, Price = 50, CreationDate = DateTime.Now, UpdateDate = DateTime.Now }
         };
 
             mockProductRead.Setup(service => service.GetAll(false)).Returns(products.AsQueryable());
@@ -91,12 +56,14 @@ namespace xUnitTest
             //var returnedValue = okResult.Value as dynamic;
             var returnedValue = JObject.Parse(jsonString);
             Assert.NotNull(returnedValue);
-       
+
             Assert.Equal(products.Count, (int)returnedValue["totalCount"]);
             Assert.Equal(products
                 .Skip(pagination.Page * pagination.Size)
                 .Take(pagination.Size)
                 .Count(), ((IEnumerable<dynamic>)returnedValue["products"]).Count());
+
+
         }
 
     }
