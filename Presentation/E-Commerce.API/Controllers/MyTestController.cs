150a836e00a4d1cfab5e60924274c2f2787cb57d
Gecher Emre <emre.gecher@indusoft.ru>
2024-08-27 14:29:05 +0300

    Error handling and logging added to testcontroller.

--- Presentation/E-Commerce.API/Controllers/MyTestController.cs
+++ Presentation/E-Commerce.API/Controllers/MyTestController.cs
@@ -5,8 +5,10 @@ using E_Commerce.Application.ViewModels;
 using E_Commerce.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using System.Linq;
 using System.Net;
+
 
 namespace E_Commerce.API.Controllers
 {
@@ -67,102 +69,127 @@ namespace E_Commerce.API.Controllers
         [HttpGet("productslist")]
         public async Task<IActionResult> Get([FromQuery] Pagination pagination) //FromQuery fronttan query ile gelecegi icin tanimlandi.
         {
-            //await Task.Delay(1000); // Burda response biraz beklettik.Spinneri yakalaya bilmek icin.
-            /*
-             *pageleme isleminde clientin kactane veri var bilmesi izin olusturulmus degisken.
-             *Items per page sabit kaliyor calismiyor. totali bilmmemiz lazim. 
-             *ProductService de list icinde nasil karsilanacagi gorulerbilir.
-             */
-            //var totalCount = await _productRead.GetAll(false).CountAsync();
-
-            //var products = await _productRead.GetAll(false).Select(p => new
-            //{
-            //    p.Id,
-            //    p.Name,
-            //    p.Stock,
-            //    p.Price,
-            //    p.CreationDate,
-            //    p.UpdateDate
-            //})
-            //    .Skip(pagination.Page * pagination.Size)
-            //    .Take(pagination.Size).ToListAsync();
-
-            ///*once skip onra take olmali*/
-            ///*Skip atlar. 1*10 da ilk onu alir. 3*10 da 30 tane alir. bu sebeple carpma kullanildi. skipde size kadarini getir dedik.*/
-            ///*yani size 10 sa ve page 2 ise son ve ilk onluyu atlayip ortadaki 10luyu getirceke ama size kadar.*/
-                   
-            //return Ok(new
-            //{
-            //    products,
-            //    totalCount
-
-            //});
-
-            if (_unitofWork == null)
+            try
             {
-                throw new Exception("UnitOfWork is not initialized.");
+                //await Task.Delay(1000); // Burda response biraz beklettik.Spinneri yakalaya bilmek icin.
+                /*
+                 *pageleme isleminde clientin kactane veri var bilmesi izin olusturulmus degisken.
+                 *Items per page sabit kaliyor calismiyor. totali bilmmemiz lazim. 
+                 *ProductService de list icinde nasil karsilanacagi gorulerbilir.
+                 */
+                //var totalCount = await _productRead.GetAll(false).CountAsync();
+
+                //var products = await _productRead.GetAll(false).Select(p => new
+                //{
+                //    p.Id,
+                //    p.Name,
+                //    p.Stock,
+                //    p.Price,
+                //    p.CreationDate,
+                //    p.UpdateDate
+                //})
+                //    .Skip(pagination.Page * pagination.Size)
+                //    .Take(pagination.Size).ToListAsync();
+
+                ///*once skip onra take olmali*/
+                ///*Skip atlar. 1*10 da ilk onu alir. 3*10 da 30 tane alir. bu sebeple carpma kullanildi. skipde size kadarini getir dedik.*/
+                ///*yani size 10 sa ve page 2 ise son ve ilk onluyu atlayip ortadaki 10luyu getirceke ama size kadar.*/
+
+                //return Ok(new
+                //{
+                //    products,
+                //    totalCount
+
+                //});
+
+                if (_unitofWork == null)
+                {
+                    Log.Warning("UnitOfWork is not initialized");
+                    throw new Exception("UnitOfWork is not initialized.");
+                }
+
+                if (_unitofWork.ProductReadRepository == null)
+                {
+                    Log.Warning("UnitOfWork is not initialized");
+                    throw new Exception("ProductReadRepository could not be initialized.");
+                }
+
+                /*totalCount ve product degisken isimleri clientta karsi ayni ismde olmaz ise veri cekilemiyor.*/
+                /*Clinetda listComponent icindeki " const allProducts: { totalCount: number, products: List_Products[] } " */
+                var totalCount = await _unitofWork.ProductReadRepository.GetAll(false).CountAsync();
+                var products = await _unitofWork.ProductReadRepository.GetAll(false).Select(p => new
+                {
+                    p.Id,
+                    p.Name,
+                    p.Stock,
+                    p.Price,
+                    p.CreationDate,
+                    p.UpdateDate
+                })
+                   .Skip(pagination.Page * pagination.Size)
+                   .Take(pagination.Size).ToListAsync();
+
+                return Ok(new
+                {
+                    products,
+                    totalCount
+                });
             }
-
-            if (_unitofWork.ProductReadRepository == null)
+            catch (Exception ex)
             {
-                throw new Exception("ProductReadRepository could not be initialized.");
+                Log.Error(ex, " List operation could not be initialized ");
+                throw new InvalidOperationException("List operation could not be initialized " + ex.Message);
             }
-
-            /*totalCount ve product degisken isimleri clientta karsi ayni ismde olmaz ise veri cekilemiyor.*/
-            /*Clinetda listComponent icindeki " const allProducts: { totalCount: number, products: List_Products[] } " */
-            var totalCount = await _unitofWork.ProductReadRepository.GetAll(false).CountAsync();
-            var products = await _unitofWork.ProductReadRepository.GetAll(false).Select(p => new
-            {
-                p.Id,
-                p.Name,
-                p.Stock,
-                p.Price,
-                p.CreationDate,
-                p.UpdateDate
-            })
-               .Skip(pagination.Page * pagination.Size)
-               .Take(pagination.Size).ToListAsync();
-
-            return Ok(new
-            {
-                products,
-                totalCount
-            });
 
         }
 
         [HttpGet("products/{id}")]
         public async Task<IActionResult> GetByID(string id)
         {
-            //Read oldugu icin tracking false
-            return Ok(await _productRead.GetByIdAsync(id, false));
+            try
+            {
+                //Read oldugu icin tracking false
+                return Ok(await _unitofWork.ProductReadRepository.GetByIdAsync(id, false));
 
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, " Product could not get by id ");
+                throw new InvalidOperationException("Product could not get by id : " + ex.Message);
+            }
         }
 
         [HttpPost("newprd")]
         public async Task<IActionResult> InsertProduct(VM_Create_Product model)
         {
 
-
-            //await _productWrite.AddAsync(new()
-            //{
-            //    Name = model.Name,
-            //    Stock = model.Stock,
-            //    Price = model.Price,
-            //});
-            //await _productWrite.SaveAsync();
-
-
-            await _unitofWork.ProductWriteRepository.AddAsync(new()
+            try
             {
-                Name = model.Name,
-                Stock = model.Stock,
-                Price = model.Price,
-            });
-            await _unitofWork.SaveAsync();
-            //int cast ettik.Cevidik.201 donecek OK() da olabilirdi.
-            return StatusCode((int)HttpStatusCode.Created);
+                //await _productWrite.AddAsync(new()
+                //{
+                //    Name = model.Name,
+                //    Stock = model.Stock,
+                //    Price = model.Price,
+                //});
+                //await _productWrite.SaveAsync();
 
+
+                await _unitofWork.ProductWriteRepository.AddAsync(new()
+                {
+                    Name = model.Name,
+                    Stock = model.Stock,
+                    Price = model.Price,
+                });
+                await _unitofWork.SaveAsync();
+                //int cast ettik.Cevidik.201 donecek OK() da olabilirdi.
+                return StatusCode((int)HttpStatusCode.Created);
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occured while creating product");
+                throw new InvalidOperationException("An error occured while creating product" + ex.Message);
+            }
 
         }
 
@@ -170,26 +197,48 @@ namespace E_Commerce.API.Controllers
         [HttpPut("updateProduct")]
         public async Task<IActionResult> UpdateProduct(VM_Update_Product model)
         {
-            //Id yi ayri olarak almak yerine once id si esit olani cekip sonra update islemi yaptik.
-            //update fonk kullanmadik cunku EF de verinin contexten cekilmemsi update icin yeterlidir cunku islem track ediliyor.
-            //SaveChanges yapilarak kayit edilir. 
-            Product product = await _productRead.GetByIdAsync(model.Id);
-            product.Stock = model.Stock;
-            product.Price = model.Price;
-            product.Name = model.Name;
-            await _productWrite.SaveAsync();
+            try
+            {
+                //Id yi ayri olarak almak yerine once id si esit olani cekip sonra update islemi yaptik.
+                //update fonk kullanmadik cunku EF de verinin contexten cekilmemsi update icin yeterlidir cunku islem track ediliyor.
+                //SaveChanges yapilarak kayit edilir. 
 
-            return Ok();
+                //Product product = await _productRead.GetByIdAsync(model.Id);
+                Product product = await _unitofWork.ProductReadRepository.GetByIdAsync(model.Id);
+                product.Stock = model.Stock;
+                product.Price = model.Price;
+                product.Name = model.Name;
+                //await _productWrite.SaveAsync();
+                await _unitofWork.SaveAsync();
 
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occured while updating product");
+                throw new InvalidOperationException("An error occured while updating product" + ex.Message);
+            }
         }
 
         [HttpDelete("deleteproduct/{id}")]
         public async Task<IActionResult> Delete(string id)
         {
-            await _productWrite.RemoveAsync(id);
-            await _productWrite.SaveAsync();
+            try
+            {
+                //await _productWrite.RemoveAsync(id);
+                //await _productWrite.SaveAsync();
 
-            return Ok();
+                await _unitofWork.ProductWriteRepository.RemoveAsync(id);
+                await _unitofWork.SaveAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occured while deleting product");
+                throw new InvalidOperationException("An error occured while deleting product" + ex.Message);
+            }
         }
 
         //Bir post fonk oldugu icin birbirinden ayirmamiz gerekiyor bu sebeple action ekledik. angularda belirlenen actionun ismi gelecek.s
@@ -227,8 +276,8 @@ namespace E_Commerce.API.Controllers
             }
             catch (Exception ex)
             {
-
-                throw new InvalidOperationException(ex.Message);
+                Log.Error(ex, "An error occured while uploading file!");
+                throw new InvalidOperationException("An error occured while uploading file!" + ex.Message);
             }
         }
     }
